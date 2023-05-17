1c9517d80d62d6289257ce61019dcf23a386dc92
Sabin <sabin@USER-no-MacBook-Air.local>
2023-05-17 13:01:21 +0800

    starter code

---
+++ Develop/index.js
@@ -0,0 +1,26 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What is the name of your project?",
+    "Write a brief description about your project.",
+    "What are the installation instruction for your project?",
+    "Are there any prerequisites or dependencies required for your project?",
+    "How can someone use or contribute to your project?",
+    "Are there any examples or usage instructions you would like to provide?",
+    "What license is your project distributed under?",
+    "Is there any additional documentation or resources you would like to include?",
+    "How can someone report issues or provide feedback on your project?",
+    "Are there any acknowledgments or credits you would like to mention?"
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
