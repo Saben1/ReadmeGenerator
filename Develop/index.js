96166aae5a00fbf26d31ae8e5cf60b9acfcaf6c6
Sabin <sabin@USER-no-MacBook-Air.local>
2023-05-17 13:27:39 +0800

    updated questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,16 +4,57 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is the name of your project?",
-    "Write a brief description about your project.",
-    "What are the installation instruction for your project?",
-    "Are there any prerequisites or dependencies required for your project?",
-    "How can someone use or contribute to your project?",
-    "Are there any examples or usage instructions you would like to provide?",
-    "What license is your project distributed under?",
-    "Is there any additional documentation or resources you would like to include?",
-    "How can someone report issues or provide feedback on your project?",
-    "Are there any acknowledgments or credits you would like to mention?"
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the project title?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project: ",
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process if any: ",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "What is this project usage for?",
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "Choose the appropriate license for this project: ",
+        choices: ["Apache", "Academic", "GNU", "ISC", "MIT", "Mozilla", "Open"],
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message: "Who are the contributors of this project?",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "Is there a test included?",
+      },
+      {
+        type: "input",
+        name: "questions",
+        message: "What do I do if I have an issue?",
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "Please enter your GitHub username: ",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter your email: ",
+      },
 ];
 
 // TODO: Create a function to write README file
