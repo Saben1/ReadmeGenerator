11bcdbbfd64bc71fe40c7baeac1f35b54ec98743
Sabin <sabin@USER-no-Air.hub>
2023-05-22 16:18:51 +0800

    updated with user input

--- Develop/index.js
+++ Develop/index.js
@@ -2,66 +2,106 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type: "input",
-        name: "projectTitle",
-        message: "What is the project title?",
-      },
-      {
-        type: "input",
-        name: "description",
-        message: "Write a brief description of your project: ",
-      },
-      {
-        type: "input",
-        name: "installation",
-        message: "Describe the installation process if any: ",
-      },
-      {
-        type: "input",
-        name: "usage",
-        message: "What is this project usage for?",
-      },
-      {
-        type: "list",
-        name: "license",
-        message: "Choose the appropriate license for this project: ",
-        choices: ["Apache", "Academic", "GNU", "ISC", "MIT", "Mozilla", "Open"],
-      },
-      {
-        type: "input",
-        name: "contributing",
-        message: "Who are the contributors of this project?",
-      },
-      {
-        type: "input",
-        name: "tests",
-        message: "Is there a test included?",
-      },
-      {
-        type: "input",
-        name: "questions",
-        message: "What do I do if I have an issue?",
-      },
-      {
-        type: "input",
-        name: "username",
-        message: "Please enter your GitHub username: ",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Please enter your email: ",
-      },
-];
+      name: 'projectTitle',
+      type: 'input',
+      message: 'Enter the title of your project:',
+    },
+    {
+      name: 'description',
+      type: 'input',
+      message: 'Enter a description for your project:',
+    },
+    {
+      name: 'installation',
+      type: 'input',
+      message: 'Enter installation instructions:',
+    },
+    {
+      name: 'usage',
+      type: 'input',
+      message: 'Enter usage information:',
+    },
+    {
+      name: 'license',
+      type: 'list',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+      name: 'contributing',
+      type: 'input',
+      message: 'Enter contribution guidelines:',
+    },
+    {
+      name: 'tests',
+      type: 'input',
+      message: 'Enter test instructions:',
+    },
+    {
+      name: 'githubUsername',
+      type: 'input',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      name: 'email',
+      type: 'input',
+      message: 'Enter your email address:',
+    },
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('README.md file generated successfully.');
+      }
+    });
+  }
+  
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+      // Generate the README content using the user's answers
+      const readmeContent = `
+  # ${answers.projectTitle}
+  
+  ## ${answers.description}
+  ...
+  
+  ## ${answers.installation}
+  ...
+  
+  ## ${answers.usage}
+  ...
+  
+  ## ${answers.license}
+  ...
+  
+  ## ${answers.contributing}
+  ...
+  
+  ## ${answers.tests}
+  ...
+  
+  ## ${answers.githubUsername}
+  ...
+
+  ## ${answers.email}
+  ...
+  `;
+  
+      // Write the README file
+      writeToFile('README.md', readmeContent);
+    });
+  }
+  
 
 // Function call to initialize app
 init();
